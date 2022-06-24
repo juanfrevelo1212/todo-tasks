6c319e5bada75ce3cf766a44eecd239730671059
juanfrevelo1212 <108039252+juanfrevelo1212@users.noreply.github.com>
2022-06-23 21:02:04 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const app = express();
+const api =require('./api');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+app.listen(port, function () {
+    console.log("Server is listening at port: " + port);
+});
+
+app.get('/', function (req, res) {
+    res.send("hello worldddd");
+});
+app.use('/api', api);
